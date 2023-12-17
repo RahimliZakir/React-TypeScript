41552463e5880166ad2e4977b8a59a39657ff9fd
RahimliZakir <zakirer@code.edu.az>
2023-12-17 03:15:12 +0100

    useRef Hook #2

--- app/src/components/DomRef.tsx
+++ app/src/components/DomRef.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useRef } from "react";
 
 const DomRef = () => {
-  //* "null!" it is "non-null" assertion.
+  //* 1) "null!" it is "non-null" assertion.
   const inputRef = useRef<HTMLInputElement>(null!);
+  //* 2) "null".
+  //   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
+    //* 1)
     inputRef.current.focus();
+    //* 2)
+    // inputRef.current?.focus();
   }, []);
 
   return (
